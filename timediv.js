28ffe31200a8c547f20f476f2e935ca7c1058610
Selbin_C_R <selbinzr@gmail.com>
2020-06-06 18:14:59 +0530

    Refactored code

--- timediv.js
+++ timediv.js
@@ -24,7 +24,6 @@ const timeDivider = (startTime, endTime) => {
   startTime = startDate.setHours(0, 0, 0, 0)
   const endDate = new Date(endTime)
   endTime = endDate.setHours(23, 59, 59, 999)
-  console.log(new Date(startTime), new Date(endTime))
   while (startTime < endTime) {
     startTime = _monthSplitter(startTime, endTime, time) || _weekSplitter(startTime, endTime, time) || _daySplitter(startTime, endTime, time)
   }
@@ -47,17 +46,17 @@ const _monthSplitter = (startTime, endTime, time) => {
   const startDate = new Date(startTime)
   const month = startDate.getMonth() + 1
   if (!(startDate.getDate() === 1)) return null
-  const daysInMonth = new Date(startDate.getFullYear(), month - 1, 0).getDate() - 1
-  if (!_isTimeIncluded(startTime, endTime, daysInMonth)) return null
+  const monthSkip = (new Date(startDate.getFullYear(), month, 0).getDate() - 1) * 8.64e+7
+  if (!_isTimeIncluded(startTime, endTime, monthSkip)) return null
   weekFlag = false
   dayFlag = false
   if (!monthFlag) {
     monthFlag = true
-    time.monthly.push({ start: startTime, end: startTime + daysInMonth + skipDay - 1 })
+    time.monthly.push({ start: startTime, end: startTime + monthSkip + skipDay - 1 })
   } else {
-    time.monthly[time.monthly.length - 1].end = startTime + daysInMonth + skipDay - 1
+    time.monthly[time.monthly.length - 1].end = startTime + monthSkip + skipDay - 1
   }
-  return startTime + daysInMonth + skipDay
+  return startTime + monthSkip + skipDay
 }
 
 /**
@@ -72,9 +71,9 @@ const _monthSplitter = (startTime, endTime, time) => {
 const _weekSplitter = (startTime, endTime, time) => {
   const startDate = new Date(startTime)
   const month = startDate.getMonth() + 1
-  const lastDay = new Date(startDate.getFullYear(), month - 1, 0)
-  const remainingDays = lastDay.getDate() - 28 ? (lastDay.getDate() - 28 * skipDay) + skipDay - 1 : skipDay - 1
-  const weekStart = [1, 8, 15, 22]
+  const lastDay = new Date(startDate.getFullYear(), month, 0).getDate()
+  const remainingDays = lastDay - 28 ? ((lastDay - 28) * skipDay) : skipDay - 1
+  const weekStart = [1, 8, 15, 22] // week should start with following
   if (!(weekStart.includes(startDate.getDate()))) return null
   if (!_isTimeIncluded(startTime, endTime, skipWeek)) return null
   dayFlag = monthFlag = false
@@ -84,7 +83,7 @@ const _weekSplitter = (startTime, endTime, time) => {
   } else {
     time.weekly[time.weekly.length - 1].end = startTime + skipWeek + skipDay - 1
   }
-  if (new Date(time.weekly[time.weekly.length - 1].end).getDate() === 28) {
+  if (new Date(time.weekly[time.weekly.length - 1].end).getDate() === 28) { // special case add remaining days
     if (time.weekly[time.weekly.length - 1].end + remainingDays <= endTime) {
       time.weekly[time.weekly.length - 1].end = time.weekly[time.weekly.length - 1].end + remainingDays
       return time.weekly[time.weekly.length - 1].end + 1
@@ -114,6 +113,8 @@ const _daySplitter = (startTime, endTime, time) => {
   return startTime + skipDay
 }
 
-const _isTimeIncluded = (startTime, endTime, time) => endTime - startTime >= skipDay - 1
+const _isTimeIncluded = (startTime, endTime, time) => endTime - startTime >= time
+
+timeDivider(1581145671000, 1582960071000)
 
 module.exports = timeDivider
